d314f08f1a405f46b569425890de1d7a249b4146
Kimope <emonteroperez@gmail.com>
2015-01-15 00:15:00 +0100

    Añadidos atributos de registros

--- src/agendavital/vista/FXMLRegistroPreguntaDosController.java
+++ src/agendavital/vista/FXMLRegistroPreguntaDosController.java
@@ -15,7 +15,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -59,6 +62,12 @@ public class FXMLRegistroPreguntaDosController implements Initializable {
     private Label nombrelabel;
     @FXML
     private ImageView imgImagen; 
+    @FXML
+    private TextArea txtDescripcion;
+    @FXML
+    private DatePicker dpFecha;
+    @FXML
+    private TextField txtTitulo;
 
     /**
      * Initializes the controller class.

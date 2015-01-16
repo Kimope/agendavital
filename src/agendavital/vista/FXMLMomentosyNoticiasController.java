556fe79ed133f49d99a7c8d48afc18be322b7d26
rrgonzalez1992 <rrgonzalez1992@hotmail.com>
2015-01-16 18:39:16 +0100

    Agregada noticias 1976 - 1978

--- src/agendavital/vista/FXMLMomentosyNoticiasController.java
+++ src/agendavital/vista/FXMLMomentosyNoticiasController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;

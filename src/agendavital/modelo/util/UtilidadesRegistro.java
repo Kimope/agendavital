90e849afd5ecf2e93f817785fc3375ebd733e96b
rrgonzalez1992 <rrgonzalez1992@hotmail.com>
2015-01-16 18:43:42 +0100

    ....

--- src/agendavital/modelo/util/UtilidadesRegistro.java
+++ src/agendavital/modelo/util/UtilidadesRegistro.java
@@ -136,7 +136,7 @@ public class UtilidadesRegistro {
             case 1965:
                 return "fallece, en Londres, el escritor Thomas S. Eliot?";
             case 1966:
-                return "EEUU lanzo a la Luna el primer cohete automatico?";
+                return "el Real Madrid ganó su sexta Copa de Europa?";
             case 1967:
                 return "comenzo la guerra de los seis dias?";
             case 1968:
@@ -152,19 +152,87 @@ public class UtilidadesRegistro {
             case 1973:
                 return "Reino Unido permitio operar en bolsa a las mujeres?";
             case 1974:
-                return "dimitio Richard Nixon, presidente de EEUU?";
+                return "dimitió Richard Nixon, presidente de EEUU?";
             case 1975:
-                return "murio Francisco Franco?";
+                return "murió Francisco Franco?";
             case 1976:
                 return "el Rey nombro a Adolfo Suarez nuevo presidente del gobierno?";
             case 1977:
-                return "se legalizo el Partido Comunista?";
+                return "se legalizó el Partido Comunista?";
             case 1978:
-                return "se proclamo nuestra constitucion?";
+                return "se proclamó la constitucion Española?";
             case 1979:
                 return "se celebraron, 4 decadas despues, elecciones municipales?";
             case 1980:
-                return "";
+                return "España y el Reino Unido reabrieron la frontera entre Gibraltar y España?";
+            case 1981:
+                return "falló el intento de golpe de Estado en España(23-F)?";
+            case 1982:
+                return "se implantó con exito el primer corazón artificial en EEUU?";
+            case 1983:
+                return "murió el pintor español Joan Miró?";
+            case 1984:
+                return "nacio Mark Zuckerberg ,creador de Facebook?";
+            case 1985:
+                return "se encontraron los restos del Titanic?";
+            case 1986:
+                return "ocurrió la catástrofe nuclear de Chernóbil?";
+            case 1987:
+                return "nació el futbolista Argentino Lionel Messi?";
+            case 1988:
+                return "nació la cantante Estadounidense Rihanna?";
+            case 1989:
+                return "cayó el muro de Berlin?";   
+            case 1990:
+                return "Se liberó a Nelson Mandela tras 27 años de cautiverio?";
+            case 1991:
+                return "falleció Freddie Mercury a causa del SIDA?";
+            case 1992:
+                return "tuvieron lugar en España las Olimpiadas de Barcelona?";
+            case 1993:
+                return "nació el piloto español Marc Márquez?";
+            case 1994:
+                return "La seleccion de Futbol de Brasil ganó su cuarto Mundial?";
+            case 1995:
+                return "Austria,Finlandia y Suecia ingresaron en la Unión Europea?";
+            case 1996:
+                return "la banda armada ETA secuestró a José Antonio Ortega Lara?";
+            case 1997:
+                return "nació Dolly, la primera oveja clonada?";
+            case 1998:
+                return "se inicio el 'boom' de la telefonía movil?";
+            case 1999:
+                return "tomo posesion el presidente electo de Venezuela Hugo Chavez?";
+            case 2000:
+                return "Se extendió la crisis de las vacas locas?";
+            case 2001:
+                return "La banda terrorista Al-Qaeda atacó a las torres Gemelas en EEUU?";
+            case 2002:
+                return "el petrolero 'Prestige' provocó un desastre naturan en España?";
+            case 2003:
+                return "tuvo lugar el brote de gripe aviar en Asia?";
+            case 2004:
+                return "la banda terrorista Al-Qaeda atentó contra 4 trenes en España?";
+            case 2005:
+                return "el huracan Katrina asoló la ciudad de Nueva Orleans?";
+            case 2006:
+                return "se realizó el primer transplante de cara?";
+            case 2007:
+                return "falleció el futbolista Antonio Puerta durante un partido?";
+            case 2008:
+                return "se inició la crisis económica y financiera mundial?";
+            case 2009:
+                return "murió Michael Jackson?";
+            case 2010:
+                return "la selección española de Futbol logró ganar su primer mundial?";
+            case 2011:
+                return "EEUU capturó y mató al lider de Al-Qaeda Osama Bin Laden?";
+            case 2012:
+                return "falleció la cantante estadounidense Whitney Houston?";
+            case 2013:
+                return "cayó un meteorito en Rusia?";
+            case 2014:
+                return "un grupo Yihadista atacó una redacción en Paris?";
             default:
                 return "";
         }
